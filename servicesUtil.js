16ae42f0657e1ea35c4da86fc64731ee81ddb697
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-03-26 00:45:25 -0400

    fixing app port environment variable

--- servicesUtil.js
+++ servicesUtil.js
@@ -31,7 +31,7 @@ async function createToken() {
 async function createLink(email, token) {
   try {
 
-    const verificationLink = `http://${process.env.SENDER_DOMAIN}:${APP_PORT}/verify?email=${encodeURIComponent(email)}&token=${encodeURIComponent(token)}`;
+    const verificationLink = `http://${process.env.SENDER_DOMAIN}:${process.env.APP_PORT}/verify?email=${encodeURIComponent(email)}&token=${encodeURIComponent(token)}`;
     return verificationLink;
   } catch (error) {
     console.error('Error creating link:', error);
